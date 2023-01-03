ada14ef881b324c70da39d9b2a97bf5e172c14f3
IrraPratiwi <pratiwiirra@gmail.com>
2023-01-03 23:16:48 +0700

    Irra menambahkan packages Cobain untuk GUI

---
+++ src/Cobain/Main.java
@@ -0,0 +1,21 @@
+package Cobain;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Vivobook
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        JFrame frame = new JFrame("My Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Menampilkan frame
+    frame.setVisible(true);
+  }
+}
+       
