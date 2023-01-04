c679752898a11151d57876cfe735b0b03f6f5736
Zulfarahmani <zulfarahmani8888@gmail.com>
2023-01-04 11:28:08 +0700

    Zulfa menambahkan class Main

---
+++ src/Cobain/whack_a_mole/Main.java
@@ -0,0 +1,17 @@
+package whack_a_mole;
+
+/**
+ *
+ * @author Vivobook
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        Game frame = new Game();
+        frame.setVisible(true);
+       
+  }
+}
